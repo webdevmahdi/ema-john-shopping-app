945ef8a58e9d4cbc0de42f081d17202118ab59c3
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-30 13:21:58 +0600

    login page setup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,8 +3,18 @@ import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className="form-parent">
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor="Email">Email</label>
+                    <input type="email" placeholder='Enter email' required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder='Enter password' required />
+                </div>
+            </form>
         </div>
     );
 };
